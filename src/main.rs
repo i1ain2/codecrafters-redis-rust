5dabb799b1b7819f2028f76b1595317c2e069d40
i1ain2 <skrkyoko@gmail.com>
2023-01-12 12:12:48 +0900

    modify stage #3

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@ use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn handle_client(mut stream: &TcpStream) -> std::io::Result<()> {
-    let mut buf = [0; 512];
-    let size = stream.read(&mut buf)?;
-    let req = String::from_utf8(buf[..size].to_vec()).unwrap();
-    match &*req {
-        // "PING" => {
-        //     stream.write(b"+PONG\r\n")?;
-        // }
-        // _ => passprintln!("not matched: {}", req),
-        _ => {
-            stream.write(b"+PONG\r\n")?;
-            stream.flush()?;
+    loop {
+        let mut buf = [0; 512];
+        let size = stream.read(&mut buf)?;
+        if size == 0 {
+            break;
+        }
+        let req = String::from_utf8(buf[..size].to_vec()).unwrap();
+        match &*req {
+            // "PING" => {
+            //     stream.write(b"+PONG\r\n")?;
+            // }
+            // _ => passprintln!("not matched: {}", req),
+            _ => {
+                stream.write(b"+PONG\r\n")?;
+                stream.flush()?;
+            }
         }
     }
     Ok(())
@@ -22,9 +27,9 @@ fn main() {
     let listerner = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listerner.incoming() {
         match stream {
-            Ok(mut _stream) => loop {
+            Ok(mut _stream) => {
                 let _ = handle_client(&_stream);
-            },
+            }
             Err(e) => {
                 panic!("ecountered IO error: {e}")
             }
