e3249f8ddbf9c26eb4cbbc6f2e42f57e7393c8bc
i1ain2 <skrkyoko@gmail.com>
2023-01-12 12:05:12 +0900

    pass stage #3

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-use std::io::prelude::*;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(mut stream: &TcpStream) -> () {
-    let _ = stream.write(b"+PONG\r\n");
+fn handle_client(mut stream: &TcpStream) -> std::io::Result<()> {
+    let mut buf = [0; 512];
+    let size = stream.read(&mut buf)?;
+    let req = String::from_utf8(buf[..size].to_vec()).unwrap();
+    match &*req {
+        // "PING" => {
+        //     stream.write(b"+PONG\r\n")?;
+        // }
+        // _ => passprintln!("not matched: {}", req),
+        _ => {
+            stream.write(b"+PONG\r\n")?;
+            stream.flush()?;
+        }
+    }
+    Ok(())
 }
 
 fn main() {
     let listerner = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listerner.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                handle_client(&_stream);
-            }
+            Ok(mut _stream) => loop {
+                let _ = handle_client(&_stream);
+            },
             Err(e) => {
-                println!("error: {}", e);
+                panic!("ecountered IO error: {e}")
             }
         }
     }
